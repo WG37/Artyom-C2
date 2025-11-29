670fb63b0bc486506587b3217e6098de7b18ddca
WG37 <willg2070@gmail.com>
2025-11-29 11:51:20 +1100

    feat(): added whoami command

---
+++ Agent/Application/Commands/Whoami.cs
@@ -0,0 +1,17 @@
+﻿using AgentClient.Domain.Models.Agents;
+using System.Security.Principal;
+
+
+namespace AgentClient.Application.Commands
+{
+    public class Whoami : AgentCommand
+    {
+        public override string Name => "whoami";
+
+        public override string Execute(AgentTask task)
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            return identity.Name;
+        }
+    }
+}
