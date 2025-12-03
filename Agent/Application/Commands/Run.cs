59dce124605cbc9dbf78666ad7ba402dddd16fc9
WG37 <willg2070@gmail.com>
2025-12-03 15:36:32 +1100

    fix(): run cmd now returns the exec service

---
+++ Agent/Application/Commands/Run.cs
@@ -0,0 +1,17 @@
+﻿using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public class Run : AgentCommand
+    {
+        public override string Name => "run";
+
+        public override string Execute(AgentTask task)
+        {
+            var fileName = task.Arguments[0];
+            var args = string.Join(" ", task.Arguments.Skip(1));
+
+            return Services.ExecutionService.ExecuteCommand(fileName, args);
+        }
+    }
+}
