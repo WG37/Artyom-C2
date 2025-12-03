3121e24041891fbf2ee7c028bf469e97bf744b7e
WG37 <willg2070@gmail.com>
2025-12-03 15:35:56 +1100

    fix(): shell cmd now returns args to the exec service

--- Agent/Application/Commands/Shell.cs
+++ Agent/Application/Commands/Shell.cs
@@ -1,5 +1,4 @@
 ﻿using AgentClient.Domain.Models.Agents;
-using System.Diagnostics;
 
 namespace AgentClient.Application.Commands
 {
@@ -9,34 +8,9 @@ namespace AgentClient.Application.Commands
 
         public override string Execute(AgentTask task)
         {
-            var stdout = "";
-
             var args = string.Join(" ", task.Arguments);
 
-            var pStartInfo = new ProcessStartInfo
-            {
-                FileName = @"C:\Windows\System32\cmd.exe",
-                Arguments = $"/c {args}",
-                WorkingDirectory = Directory.GetCurrentDirectory(),
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            var process = Process.Start(pStartInfo);
-
-            using (process.StandardOutput)
-            {
-                stdout += process.StandardOutput.ReadToEnd();
-            }
-
-            using (process.StandardError)
-            {
-                stdout += process.StandardError.ReadToEnd();
-            }
-
-            return stdout;
+            return Services.ExecutionService.ExecuteCommand(@"C:\Windows\System32\cmd.exe", $"/c {args}");
         }
     }
 }
