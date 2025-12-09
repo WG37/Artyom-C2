392cf8458eb507a28b974624d99a6e430cb280d5
WG37 <willg2070@gmail.com>
2025-12-09 20:03:58 +1100

    feat(): added new test assembly project

---
+++ AssemblyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assembly loaded");
+        }
+    }
+}
