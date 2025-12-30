8c5684c2caeaac69e876e18af9ec1d86402f19b3
WG37 <willg2070@gmail.com>
2025-12-30 13:10:12 +1100

    add migrations

---
+++ TeamServer/Migrations/20251230015234_addListenerStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addListenerStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ListenerStatus",
+                table: "HttpListeners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ListenerStatus",
+                table: "HttpListeners");
+        }
+    }
+}
