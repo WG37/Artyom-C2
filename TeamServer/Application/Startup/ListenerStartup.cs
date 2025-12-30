0b34908773feed290b3344fc6d046a4ed9e9aae3
WG37 <willg2070@gmail.com>
2025-12-30 13:09:20 +1100

    feat(): add status caller

---
+++ TeamServer/Application/Startup/ListenerStartup.cs
@@ -0,0 +1,34 @@
+﻿
+using TeamServer.Application.Services.ListenerServices.HttpListenerService.HttpCRUD;
+
+namespace TeamServer.Application.Startup
+{
+    public sealed class ListenerStartup : IHostedService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ListenerStartup(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var crud = scope.ServiceProvider.GetRequiredService<IHttpCRUD>();
+
+            var listeners = await crud.GetAllListenersAsync();
+            foreach (var l in listeners)
+            {
+                l.Idle();
+            }
+
+            await crud.UpdateChangesAsync(listeners);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
