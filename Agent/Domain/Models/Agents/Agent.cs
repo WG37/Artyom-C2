9751928869ccf90c58f4c7fa8ed50fc0ddecfe34
WG37 <willg2070@gmail.com>
2025-12-30 13:09:32 +1100

    chore(): line spacing

--- Agent/Domain/Models/Agents/Agent.cs
+++ Agent/Domain/Models/Agents/Agent.cs
@@ -3,9 +3,7 @@
     public class Agent
     {
         public Guid UniqueId { get; set; } = Guid.NewGuid();
-
         public AgentMetadata Metadata { get; set; }
-
         public Agent(AgentMetadata metadata)
         {
             Metadata = metadata;
