f65534eb11038155ca57cca504245d4a989d262f
WG37 <willg2070@gmail.com>
2026-01-05 12:27:17 +1100

    feat(): add single file encryption

---
+++ Agent/Infrastructure/Cryptography/FileEncryption/FileEncryptor.cs
@@ -0,0 +1,35 @@
+﻿using AgentClient.Infrastructure.Utilities.KeyGen;
+using System.Security.Cryptography;
+
+namespace AgentClient.Infrastructure.Cryptography.FileEncryption
+{
+    public class FileEncryptor
+    {
+        private readonly byte[] _key;
+
+        public FileEncryptor(byte[] key)
+        {
+            _key = key;
+        }
+
+        public void EncryptFile(string path)
+        {
+            var pt = File.ReadAllBytes(path);
+            var iv = KeyGeneration.KeyGen(16);
+
+            using var aes = Aes.Create();
+            aes.Key = _key;
+            aes.IV = iv;
+
+            using var ms = new MemoryStream();
+            ms.Write(iv, 0, iv.Length);
+
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(pt, 0, pt.Length);
+            }
+
+            File.WriteAllBytes(path, ms.ToArray());
+        }
+    }
+}
