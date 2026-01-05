d0809caf00cdec8a8aa369c9204a1080e4f9fc94
WG37 <willg2070@gmail.com>
2026-01-05 12:32:25 +1100

    add migrations

---
+++ TeamServer/Migrations/20260105012102_addSystemUuidProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSystemUuidProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Metadata_SystemUuid",
+                table: "Agents",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Metadata_SystemUuid",
+                table: "Agents");
+        }
+    }
+}
