d86111dba05c1dbf18cde7dd6c9eb2bc3344cb91
WG37 <willg2070@gmail.com>
2026-01-05 12:26:44 +1100

    feat(): add CSPRNG KeyGen

---
+++ Agent/Infrastructure/Utilities/KeyGen/KeyGeneration.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace AgentClient.Infrastructure.Utilities.KeyGen
+{
+    public static class KeyGeneration
+    {
+        public static byte[] KeyGen(int length = 32)
+        {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            var bytes = new byte[length];
+
+            using (var csprng = RandomNumberGenerator.Create())
+            {
+                csprng.GetBytes(bytes);
+            }
+
+            return bytes;
+        }
+    }
+}
