176f57c44e4a5ff05173b22922b0550b2f858296
WG37 <willg2070@gmail.com>
2026-01-05 12:29:31 +1100

    feat(): add get Uuid method

---
+++ Agent/Infrastructure/Utilities/HWIDs/ComponentIDs/SystemUuid.cs
@@ -0,0 +1,22 @@
+﻿using System.Management;
+
+namespace AgentClient.Infrastructure.Utilities.HWIDs.ComponentIDs
+{
+    public class SystemUuid
+    {
+        public static string GetSystemUuid()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT UUID FROM Win32_ComputerSystemProduct"))
+            {
+                foreach (var obj in searcher.Get())
+                {
+                    var uuid = obj["UUID"]?.ToString() ?? "";
+
+                    return uuid;
+                }
+            }
+
+            return "";
+        }
+    }
+}
