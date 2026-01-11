b2b9602dfcdbae30e9f48ef647bf0004bb41e709
WG37 <willg2070@gmail.com>
2026-01-11 13:19:22 +1100

    feat(): add class/method for AntiVirus discovery

---
+++ Agent/Infrastructure/Native/Windows/Wmi/AntiVirusProduct/AntiVirus.cs
@@ -0,0 +1,27 @@
+﻿using System.Management;
+
+namespace AgentClient.Infrastructure.Native.Windows.Wmi.AntiVirusProduct
+{
+    public static class AntiVirus
+    {
+        public static List<(string avName, string avPath)> GetAllAntiVirus()
+        {
+            var results = new List<(string avName, string avPath)>();
+
+            var scope = new ManagementScope(@"\\.\root\SecurityCenter2");
+            var query = new ObjectQuery("SELECT * FROM AntiVirusProduct");
+
+            using (var searcher = new ManagementObjectSearcher(scope, query))
+            {
+                foreach (var obj in searcher.Get())
+                {
+                    var avName = obj["displayName"]?.ToString() ?? "";
+                    var avPath = obj["pathToSignedProductExe"]?.ToString() ?? "";
+
+                    results.Add((avName, avPath));
+                }
+            }
+            return results;
+        }
+    }
+}
