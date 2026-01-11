d055ef517d6cb1c33c8cb0d86b7180f50405cbd1
WG37 <willg2070@gmail.com>
2026-01-11 13:19:59 +1100

    feat(): add command class to get all AntiVirus products

---
+++ Agent/Application/Commands/ListAntiVirus.cs
@@ -0,0 +1,30 @@
+﻿using AgentClient.Domain.Models.Agents;
+using AgentClient.Infrastructure.Native.Windows.Wmi;
+using AgentClient.Infrastructure.Native.Windows.Wmi.AntiVirusProduct;
+
+namespace AgentClient.Application.Commands
+{
+    public class ListAntiVirus : AgentCommand
+    {
+        public override string Name => "list-av";
+
+        public override string Execute(AgentTask task)
+        {
+            var results = new List<string>();
+            
+            var avList = AntiVirus.GetAllAntiVirus();
+            
+            if (avList.Count == 0)
+            {
+                return "No antivirus products detected";
+            }
+
+            foreach (var av in avList)
+            {
+                results.Add($"{av.avName} | {av.avPath}");
+            }
+
+            return string.Join("\n", results);
+        }
+    }
+}
